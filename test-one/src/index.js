adb380cd91bcd08b2c163c29ca1ed3b7b0e2d38a
Toby Flemming <tobyf93@hotmail.com>
2017-06-14 16:40:52 +1000

    test-one: Implement algorithm

--- test-one/src/index.js
+++ test-one/src/index.js
@@ -1,11 +1,10 @@
 import Logger from './helpers/logger';
 
-let logger = new Logger();
+const logger = new Logger();
 
-//Your code should go here instead of the welcome message below. Create and modify additional files as necessary.
-
-const welcomeMessage = "Congrats, you're up and running! Welcome to the first moshtix code challenge! " +
-    "Please replace this message with your solution and feel free to add " +
-    "or modify other files as necessary. Good luck : )";
-
-logger.log(welcomeMessage);
+for (let i = 1; i < 101; i++) {
+  if (i % 15 === 0) logger.log('BossHog');
+  else if (i % 3 === 0) logger.log('Boss');
+  else if (i % 5 === 0) logger.log('Hog');
+  else logger.log(i);
+}
